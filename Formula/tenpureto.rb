9ef8cc8683de465a2338b8b814960ac1f5d05ab1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-04 23:33:44 +0000

    tenpureto: update 0.2.1 bottle.

--- Formula/tenpureto.rb
+++ Formula/tenpureto.rb
@@ -10,8 +10,8 @@ class Tenpureto < Formula
   bottle do
     root_url "https://dl.bintray.com/rtimush/bottles-tap"
     cellar :any
-    sha256 "fc04d7e3b1c766dd45da4bb101f18722f861333c1a7b7804fcd1ffcfd02e07cc" => :mojave
-    sha256 "edffe9e5807f661ebe2f1817b4b040d9e81ed44ed3ae668ceec65343959627fc" => :high_sierra
+    sha256 "9d64bbe6fc8040226a90fd7e3c9301a72c76ef8063923ed574ae8cdb60fb75dc" => :mojave
+    sha256 "edb6a6839add10bfcc5c9a6d0f8b830a6b9c025ed94471d70780eeabd3a92f3b" => :high_sierra
   end
 
   depends_on "haskell-stack" => :build
