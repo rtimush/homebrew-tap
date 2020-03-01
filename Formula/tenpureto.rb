b794def9e5807569066ce7767ffd02d1e9b06570
Roman Timushev <rtimush@gmail.com>
2020-03-01 12:23:19 +0100

    tenpureto 0.4.1

--- Formula/tenpureto.rb
+++ Formula/tenpureto.rb
@@ -2,37 +2,31 @@ class Tenpureto < Formula
   desc "Project templating tool"
   homepage "https://github.com/tenpureto/tenpureto"
 
-  url "https://github.com/tenpureto/tenpureto/archive/v0.4.0.tar.gz"
-  sha256 "bb6a6acd7932f1f1a54bf4f01a7e4ef71f9f2910e7c3d112b9042bd32e8afd1c"
-
-  head "https://github.com/tenpureto/tenpureto.git"
-
-  bottle do
-    root_url "https://dl.bintray.com/rtimush/bottles-tap"
-    cellar :any_skip_relocation
-    sha256 "81b5b8cc816b7aba8b2c8b0c01348a23b30176368f1053feb6d2557ccc7ea5e1" => :mojave
-    sha256 "2c2db13354c55ebb0b54bec2e1f0e453086c9a95b584b04d5fc43144d2e0b807" => :high_sierra
+  version "0.4.1"
+  if OS.mac?
+    url "https://github.com/tenpureto/tenpureto/releases/download/v#{version}/tenpureto-#{version}-x86_64-darwin"
+    sha256 "b675d2834714eb567f0fb6c02430c7278b892962440c33caf3726cd8d361215c"
+  else
+    url "https://github.com/tenpureto/tenpureto/releases/download/v#{version}/tenpureto-#{version}-x86_64-linux"
+    sha256 "6da809b90d6f718c4b530f48a3c7aa71239fb4da7eedc7a06e7368ccb2e3f414"
   end
 
-  depends_on "haskell-stack" => :build
+  bottle :unneeded
 
   def install
-    system "stack",
-           "--no-terminal",
-           "--jobs=#{ENV.make_jobs}",
-           "--local-bin-path=#{bin}",
-           "--extra-lib-dirs=#{HOMEBREW_PREFIX}/opt/icu4c/lib",
-           "--extra-include-dirs=#{HOMEBREW_PREFIX}/opt/icu4c/include",
-           "install"
-
+    mv "tenpureto-#{version}-x86_64-*", "tenpureto"
+    FileUtils.chmod 0755, "tenpureto"
     mkdir "completions"
-    system "sh", "-c", "#{bin}/tenpureto --bash-completion-script #{bin}/tenpureto >completions/tenpureto"
-    system "sh", "-c", "#{bin}/tenpureto --zsh-completion-script #{bin}/tenpureto >completions/_tenpureto"
+    system "sh", "-c", "./tenpureto --bash-completion-script #{bin}/tenpureto >completions/tenpureto"
+    system "sh", "-c", "./tenpureto --zsh-completion-script #{bin}/tenpureto >completions/_tenpureto"
+    bin.install "tenpureto"
     bash_completion.install "completions/tenpureto"
     zsh_completion.install "completions/_tenpureto"
   end
 
   test do
-    system "#{bin}/tenpureto", "--help"
+    actual = pipe_output("#{bin}/tenpureto --version")
+    expected = "tenpureto #{version}\n"
+    assert_equal expected, actual
   end
 end
