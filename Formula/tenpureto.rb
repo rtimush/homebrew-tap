759acef4330ae88479ebd83496dd643a1bf1fc3e
Roman Timushev <rtimush@gmail.com>
2020-03-23 01:40:33 +0100

    tenpureto 0.6.0

--- Formula/tenpureto.rb
+++ Formula/tenpureto.rb
@@ -2,13 +2,13 @@ class Tenpureto < Formula
   desc "Project templating tool"
   homepage "https://github.com/tenpureto/tenpureto"
 
-  version "0.5.1"
+  version "0.6.0"
   if OS.mac?
     url "https://github.com/tenpureto/tenpureto/releases/download/v#{version}/tenpureto-#{version}-x86_64-darwin"
-    sha256 "6da81de2bd15705a9a79ef88f50abb0aa9ec59426541b74ebed04384ac4a6a3d"
+    sha256 "044a0d0d7aa7c77232e9b23fdd514c432e798c7b700b6027698c3fe7b224cb4e"
   else
     url "https://github.com/tenpureto/tenpureto/releases/download/v#{version}/tenpureto-#{version}-x86_64-linux"
-    sha256 "2b96278f40d081d25bc31e2dc74009b411030f7c1de9688e716161ace4ea89e5"
+    sha256 "88e984d449b0163529c67f9fba0735be37bae4711c906b7311ca10070707f605"
   end
 
   bottle :unneeded
